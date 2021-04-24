69016381b6fd99a7a934635f4feeb299c4e62c14
Jan Horacek <me@apophis.cz>
2021-04-24 14:58:40 +0200

    Uni: implement RCS functions.

--- src/modules/uni.cpp
+++ src/modules/uni.cpp
@@ -2,6 +2,8 @@
 #include <QJsonObject>
 #include "uni.h"
 #include "../errors.h"
+#include "../events.h"
+#include "../main.h"
 
 namespace MtbNetLib {
 
@@ -25,12 +27,19 @@ void MtbUni::daemonGotInfo(const QJsonObject& json) {
 }
 
 void MtbUni::daemonInputsChanged(const QJsonObject& json) {
+	this->inputs = json["packed"].toInt();
+	events.call(events.onInputChanged, this->address);
 }
 
 void MtbUni::daemonOutputsChanged(const QJsonObject& json) {
+	for (const auto& key : json.keys())
+		this->outputsConfirmed[key.toInt()] = json[key].toObject();
+	events.call(events.onOutputChanged, this->address);
 }
 
 void MtbUni::daemonOutputsSet(const QJsonObject& json) {
+	for (const auto& key : json.keys())
+		this->outputsConfirmed[key.toInt()] = json[key].toObject();
 }
 
 /* RCS events --------------------------------------------------------------- */
@@ -40,6 +49,8 @@ int MtbUni::rcsGetInput(unsigned int port) {
 		return RCS_PORT_INVALID_NUMBER;
 	if (this->state != "active")
 		return RCS_MODULE_FAILED;
+
+	return (this->inputs >> port) & 0x1;
 }
 
 int MtbUni::rcsGetOutput(unsigned int port) {
@@ -47,6 +58,8 @@ int MtbUni::rcsGetOutput(unsigned int port) {
 		return RCS_PORT_INVALID_NUMBER;
 	if (this->state != "active")
 		return RCS_MODULE_FAILED;
+
+	return this->outputsConfirmed[port]["value"].toInt();
 }
 
 int MtbUni::rcsSetOutput(unsigned int port, int state) {
@@ -54,22 +67,57 @@ int MtbUni::rcsSetOutput(unsigned int port, int state) {
 		return RCS_PORT_INVALID_NUMBER;
 	if (this->state != "active")
 		return RCS_MODULE_FAILED;
+
+	QString portType = this->config["outputsSafe"].toArray()[port].toObject()["type"].toString();
+	if ((state > 1) && (portType == "plain"))
+		portType = "flicker";
+
+	daemonClient.send(QJsonObject{
+		{"command", "module_set_outputs"},
+		{"type", "request"},
+		{"address", this->address},
+		{"outputs", QJsonObject{
+			{QString::number(port), QJsonObject{
+				{"type", portType},
+				{"value", state},
+			}},
+		}},
+	});
+
+	return 0;
 }
 
 int MtbUni::rcsGetInputType(unsigned int port) {
 	if (port > UNI_IO_CNT)
 		return RCS_PORT_INVALID_NUMBER;
+
+	if (this->config.contains("irsPacked")) {
+		uint16_t irs = this->config["irsPacked"].toInt();
+		irs >>= port;
+		if (irs & 1)
+			return static_cast<int>(RcsPortInputType::iIr);
+	}
+	return static_cast<int>(RcsPortInputType::iPlain);
 }
 
 int MtbUni::rcsGetOutputType(unsigned int port) {
 	if (port > UNI_IO_CNT)
 		return RCS_PORT_INVALID_NUMBER;
+
+	const QString& portType = this->config["outputsSafe"].toArray()[port].toObject()["type"].toString();
+	if (portType == "s-com")
+		return static_cast<int>(RcsPortOutputType::oScom);
+	return static_cast<int>(RcsPortOutputType::oPlain);
 }
 
 void MtbUni::resetConfig() {
+	this->config = {};
 }
 
 void MtbUni::resetState() {
+	this->inputs = 0;
+	for (size_t i = 0; i < UNI_IO_CNT; i++)
+		this->outputsConfirmed[i] = {};
 }
 
 size_t MtbUni::inputsCount() const { return UNI_IO_CNT; }
